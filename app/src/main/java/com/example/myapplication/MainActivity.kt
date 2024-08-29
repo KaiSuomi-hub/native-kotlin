fe5a6b07d63afb2c1e6315123f1eca130bf7f4f4
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2024-08-29 20:31:03 +0300

    Seuraavaksi teht 3 ja 4 ja sitten kotityöt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import com.example.myapplication.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +30,8 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Etunimi",
+                        surname = "Sukunimi",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +40,54 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+private val LightColors = lightColorScheme(
+    primary = Color(0xFF6200EE),
+    primaryContainer = Color(0xFF3700B3),
+    secondary = Color(0xFF03DAC6)
+)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MyApplicationTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = LightColors,
+        typography = Typography,
+        content = content
     )
+}
+
+@Composable
+fun Greeting(name: String, surname: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.White) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column {
+                Text(
+                    text = "Hello, $name $surname!",
+                    modifier = modifier.padding(..dp)
+                )
+                Text(
+                    text = Count(a = 1, b = 2),
+                    modifier = modifier.padding(..dp)
+                )
+
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Count(a: Int, b: Int): String {
+    val result = a + b
+    return "Result $a + $b is $result!"
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        Greeting("Etunimi", "Developer")
     }
-    }
+}
